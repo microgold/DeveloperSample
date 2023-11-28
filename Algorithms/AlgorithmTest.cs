dc959310e1b20b0f2c91dc392c82810aedff5f61
microgold <Batman134!!>
2023-11-28 10:07:33 -0600

    Finished Algorithms

--- Algorithms/AlgorithmTest.cs
+++ Algorithms/AlgorithmTest.cs
@@ -1,19 +1,72 @@
+using System;
 using Xunit;
 
 namespace DeveloperSample.Algorithms
 {
     public class AlgorithmTest
     {
-        [Fact(Skip="Not implemented")]
+        [Fact]
         public void CanGetFactorial()
         {
             Assert.Equal(24, Algorithms.GetFactorial(4));
         }
 
-        [Fact(Skip="Not implemented")]
+        [Fact]
+        public void FactorialOfZero()
+        {
+            Assert.Equal(1, Algorithms.GetFactorial(0));
+        }
+
+        [Fact]
+        public void FactorialOfOne()
+        {
+            Assert.Equal(1, Algorithms.GetFactorial(1));
+        }
+
+        [Fact]
+        public void FactorialOfNegativeNumber()
+        {
+            Assert.Throws<ArgumentException>(() => Algorithms.GetFactorial(-1));
+        }
+
+
+
+        [Fact]
         public void CanFormatSeparators()
         {
             Assert.Equal("a, b and c", Algorithms.FormatSeparators("a", "b", "c"));
         }
+
+        [Fact]
+        public void FormatTwoSeparators()
+        {
+            Assert.Equal("a and b", Algorithms.FormatSeparators("a", "b"));
+        }
+
+        [Fact]
+        public void FormatSingleSeparator()
+        {
+            Assert.Equal("a", Algorithms.FormatSeparators("a"));
+        }
+
+        [Fact]
+        public void FormatNoSeparators()
+        {
+            Assert.Equal(string.Empty, Algorithms.FormatSeparators());
+        }
+
+        [Fact]
+        public void FormatMultipleSeparators()
+        {
+            Assert.Equal("a, b, c, d and e", Algorithms.FormatSeparators("a", "b", "c", "d", "e"));
+        }
+
+        [Fact]
+        public void FormatNullSeparators()
+        {
+            Assert.Equal(string.Empty, Algorithms.FormatSeparators(null));
+        }
+
+
     }
 }
--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -1,11 +1,40 @@
 using System;
+using System.Linq;
 
 namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+        public static int GetFactorial(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("Factorial is not defined for negative numbers.");
+
+            // Base case: the factorial of 0 or 1 is 1
+            if (n == 0 || n == 1)
+                return 1;
+
+            // Recursive case: n! = n * (n-1)!
+            return n * GetFactorial(n - 1);
+        }
+
+        public static string FormatSeparators(params string[] items)
+        {
+            if (items == null || items.Length == 0)
+                return string.Empty;
+
+            if (items.Length == 1)
+                return items[0];
+
+            if (items.Length == 2)
+                return $"{items[0]} and {items[1]}";
+
+            // Join all but the last item with a comma
+            var allButLast = string.Join(", ", items.Take(items.Length - 1));
+
+            // Add the last item with "and"
+            return $"{allButLast} and {items[^1]}";
+        }
     }
 }
